c26f727a6a12a6f5241c48752e0ff207f0c52d98
Rodrigo San Nicolas <rodrigosannicolas@gmail.com>
2023-10-26 13:33:28 -0300

    Update blankline setup

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -5,4 +5,7 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "Default", { fg = "#2C2E43" })
 end)
 
-require("ibl").setup { indent = { highlight = highlight } }
+require("ibl").setup { 
+  indent = { highlight = highlight },
+  scope = { enabled = false },
+}
