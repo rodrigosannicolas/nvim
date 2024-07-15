ff6e4b154ae4f15ed0b24e3e3f0e18fa296388d2
Rodrigo San Nicolás <rodrigosannicolas@gmail.com>
2024-07-15 14:51:40 -0300

    Fix keymap typo

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,15 +6,15 @@ return {
     "nvim-telescope/telescope-file-browser.nvim"
   },
   config = function ()
-    vim.keymaps.set("n", "<C-p>", function ()
+    vim.keymap.set("n", "<C-p>", function ()
       require("telescope").extensions.file_browser.file_browser({ path = "%:h:p", select_buffer = true })
     end)
 
-    vim.keymaps.set("n", "<leader>ps", function ()
+    vim.keymap.set("n", "<leader>ps", function ()
       require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
     end)
 
-    vim.keymaps.set("n", "<leader><leader>", function ()
+    vim.keymap.set("n", "<leader><leader>", function ()
       require("telescope.builtin").git_files()
     end)
 
