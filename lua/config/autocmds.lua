31fcb7e4a3633d04fce6bac1855f648da5e26ed2
Rodrigo San Nicolás <rodrigosannicolas@gmail.com>
2024-07-15 15:26:28 -0300

    Update autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,14 +1,8 @@
-vim.api.nvim_create_augroup('bufcheck', { clear = true })
+vim.api.nvim_create_augroup('default', { clear = true })
+
   -- Highlight when yanking (copying) text
   vim.api.nvim_create_autocmd('TextYankPost', {
-    group    = 'bufcheck',
+    group    = 'default',
     pattern  = '*',
     callback = function () vim.highlight.on_yank() end
-  })
-
-  -- Reload config file on change
-  vim.api.nvim_create_autocmd('BufWritePost', {
-    group    = 'bufcheck',
-    pattern  = vim.env.MYVIMRC,
-    command  = 'silent source %'
   })
