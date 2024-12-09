49f149f218320c47081521935f55cd6bee8f90ff
Rodrigo San Nicolás <rodrigosannicolas@gmail.com>
2024-12-09 10:32:17 -0300

    Update lualine refresh time

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -16,9 +16,9 @@ return {
         ignore_focus = {},
         always_divide_middle = true,
         refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
+          statusline = 1,
+          tabline = 1,
+          winbar = 1,
         }
       },
       sections = {
