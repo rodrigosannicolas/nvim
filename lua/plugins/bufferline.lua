dcb377e04272e57b70418dd25c0fa6f2e2237cd6
Rodrigo San Nicolás <rodrigosannicolas@gmail.com>
2025-01-23 10:57:30 -0300

    Update bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,11 +8,15 @@ return {
       options = {
         mode = "tabs", -- set to "tabs" to only show tabpages instead
         style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+        numbers = "none",
         buffer_close_icon = '󰅖',
         modified_icon = '● ',
         close_icon = ' ',
         left_trunc_marker = ' ',
         right_trunc_marker = ' ',
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        always_show_bufferline = true,
       }
     })
   end
--- lua/plugins/web-devicons.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-tree/nvim-web-devicons",
-  opts = {},
-  config = function()
-    require("nvim-web-devicons").setup({})
-  end
-}
