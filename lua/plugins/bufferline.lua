b640b869c891288c675626dd66a3d9718c3b64ab
Rodrigo San Nicolás <rodrigosannicolas@gmail.com>
2025-01-23 09:27:23 -0300

    Update bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,6 +8,11 @@ return {
       options = {
         mode = "tabs", -- set to "tabs" to only show tabpages instead
         style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+        buffer_close_icon = '󰅖',
+        modified_icon = '● ',
+        close_icon = ' ',
+        left_trunc_marker = ' ',
+        right_trunc_marker = ' ',
       }
     })
   end
