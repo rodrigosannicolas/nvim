29e2dfd683e71c1e8468b040b78e9acb7df77baa
Rodrigo San Nicolas <rodrigosannicolas@gmail.com>
2025-01-28 07:42:38 -0300

    Update copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,18 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+
+    -- Accept current suggestion with <C-J>
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false
+    })
+
+    -- dismiss current suggestion with <C-]>
+    vim.keymap.set('i', '<C-]>', 'copilot#Dismiss()', {
+      expr = true,
+      replace_keycodes = false
+    })
+  end
 }
