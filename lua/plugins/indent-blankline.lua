e64ace22e1261223f5472c346f5fef4b0e7e6535
Rodrigo San Nicolas <rodrigosannicolas@192.168.1.63>
2026-07-13 10:13:15 -0300

    Update plugins

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,9 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  config = function ()
-    require("ibl").setup({
-      scope = { enabled = false }
-    })
-  end
-}
+-- return {
+--   "lukas-reineke/indent-blankline.nvim",
+--   main = "ibl",
+--   config = function ()
+--     require("ibl").setup({
+--       scope = { enabled = false }
+--     })
+--   end
+-- }
