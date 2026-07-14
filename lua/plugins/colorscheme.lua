31ceda564dcb3049bd910dc1e5dbe22a6a618027
Rodrigo San Nicolas <rodrigosannicolas@192.168.1.63>
2026-07-14 09:45:46 -0300

    Update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,14 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  disable_background = true,
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd("colorscheme rose-pine")
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end
+    require('github-theme').setup({
+      -- ...
+    })
+
+    vim.cmd('colorscheme github_dark_default')
+  end,
 }
-
--- return {
---   'projekt0n/github-nvim-theme',
---   name = 'github-theme',
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     require('github-theme').setup({
---       -- ...
---     })
-
---     vim.cmd('colorscheme github_dark_default')
---   end,
--- }
 
