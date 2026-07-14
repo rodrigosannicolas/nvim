9486e2d8717f93f40de13971c4e3e51321fc9989
Rodrigo San Nicolas <rodrigosannicolas@192.168.1.63>
2026-07-14 09:42:37 -0300

    Remove indent-blankline

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,6 +1,6 @@
 -- PEP 8 / ruff-format default: 4-space indent, 88-char lines
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 vim.opt_local.colorcolumn = "88"
--- init.lua
+++ init.lua
@@ -2,3 +2,15 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.lazy")
+
+
+function ColorMyPencils(color) 
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
